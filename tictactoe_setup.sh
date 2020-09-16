1d1645848d6bcd2031b1f1045e49ed062741bede
J-Linares <linaresjuan727@csu.fullerton.edu>
2020-09-16 16:43:35 -0700

    updated the echo displays

--- tictactoe_setup.sh
+++ tictactoe_setup.sh
@@ -1,7 +1,7 @@
-echo "we will be creating both the dll file and .exe file for the assighnment"
+echo "we will be creating both the dll file and .exe file"
 mcs -target:library -r:System.Windows.Forms.dll -r:System.Drawing.dll -out:mytictactoeframe.dll mytictactoeframe.cs
 echo "dll file has no been created"
 mcs -r:System.Windows.Forms.dll -r:System.Drawing.dll -r:mytictactoeframe.dll -out:mytictactoe.exe mytictactoemain.cs
-echo "the .exe file for the assighnment has now been created"
+echo "the .exe file for the has now been created"
 echo "we will now run the file, if an error occurs you will be notified"
 ./mytictactoe.exe
